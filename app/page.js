e1e4e8ada30d0ea19c9508e13342c1281d1470ac
Nyenzo Isabwa <nyenzoisabwa@gmail.com>
2025-05-21 14:28:24 +0300

    Aivestor front end initialization

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Welcome to Aivestor</h1>
+    </div>
+  );
+}
